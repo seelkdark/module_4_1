fbb77457872889f4c4d31cffd67c8daf80b5f435
seelk <sedova08111984@gmail.com>
2024-11-11 16:10:00 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,18 @@
+import fake_math as fe
+import true_math as tk
+
+result1 = fe.divide(69, 3)
+
+result2 = fe.divide(3, 0)
+
+result3 = tk.divide(49, 7)
+
+result4 = tk.divide(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
